b248e5e8a647b455b6bca8e1980171ed700a3ea6
HA2001-10 <ha19971001@gmail.com>
2026-01-28 16:29:46 +0800

    Rename model from MFSC_DTI to MFDR_DTI

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import argparse
 
 from RunModel import run_model
-from model import MFSC_DTI
+from model import MFDR_DTI
 
 parser = argparse.ArgumentParser(
-    prog='MFSC_DTI',
-    description='MFSC_DTI is model in paper: \"Multi-Level Feature Extraction and Multi-Channel Shared Cross-Attention Framework for DTI prediction\"',
+    prog='MFDR_DTI',
+    description='MFDR_DTI is model in paper: \"Multi-Level Feature Extraction and Multi-Channel Shared Cross-Attention Framework for DTI prediction\"',
     epilog='Model config set by config.py')
 
 parser.add_argument('dataSetName', choices=[
                     "DrugBank","Enzyme", "GPCRs"],
                     help='Enter which dataset to use for the experiment')
-parser.add_argument('-m', '--model', choices=['MFSC_DTI'],
-                    default='MFSC_DTI', help='Which model to use, \"MFSC_DTI\" is used by default')
+parser.add_argument('-m', '--model', choices=['MFDR_DTI'],
+                    default='MFDR_DTI', help='Which model to use, \"MFDR_DTI\" is used by default')
 parser.add_argument('-s', '--seed', type=int, default=114514,
                     help='Set the random seed, the default is 114514')
 parser.add_argument('-f', '--fold', type=int, default=5,
@@ -21,9 +21,9 @@ parser.add_argument('-f', '--fold', type=int, default=5,
 args = parser.parse_args()
 
 # 修改调用部分
-if args.model == 'MFSC_DTI':
+if args.model == 'MFDR_DTI':
     run_model(SEED=args.seed,
               DATASET=args.dataSetName,
-              MODEL=MFSC_DTI,
+              MODEL=MFDR_DTI,
               K_Fold=args.fold)
 
